3b8e4dc54568053140e854bb9c34283fbba6683c
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2024-12-12 21:38:16 +0530

    Made the UI Responsive and Soft

--- src/UI/Card.jsx
+++ src/UI/Card.jsx
@@ -3,28 +3,26 @@ import React from "react";
 const Card = ({
   bannerImgSrc,
   educatorName,
-  educatorAvatar,
   courseName,
   courseType,
 }) => {
   return (
-    <div className="flex flex-col h-full bg-white rounded-lg shadow-md overflow-hidden w-full border border-neutral-200">
-      <div className="relative h-1/2 w-full p-2">
+    <div className="flex lg:flex-col h-full bg-[#1f1f1f] rounded-lg shadow-md overflow-hidden w-full border border-neutral-700">
+      <div className="relative lg:h-1/2 h-full aspect-square">
         <img
           src={bannerImgSrc}
           alt={courseName}
           className="w-full h-full object-cover"
         />
       </div>
-      <div className="p-4 flex flex-col gap-2">
+      <div className="lg:p-4 p-2 flex flex-col lg:gap-2 gap-1">
         <div className="flex items-center gap-2">
-          <img src={educatorAvatar} alt={educatorName} className="w-8 h-8 mx-1" />
-          <span className="text-gray-800 font-lg text-sm">
+          <span className="text-gray-300 font-medium text-sm">
             {educatorName}
           </span>
         </div>
-        <h3 className="text-gray-900 font-semibold text-lg">{courseName}</h3>
-        <span className="text-gray-500 text-sm py-2">{courseType}</span>
+        <h3 className="text-gray-100 font-semibold text-lg">{courseName}</h3>
+        <span className="text-gray-400 text-sm">{courseType}</span>
       </div>
     </div>
   );
--- src/app/subjects/page.jsx
+++ src/app/subjects/page.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import Card from "@/UI/Card";
+import React from "react";
 
 const page = () => {
   return (
-    <div>
-      
+    <div className="container lg:pt-24 pt-8 mx-auto p-6 text-gray-100 flex flex-col h-screen">
+      <h1 className="text-3xl font-bold text-center mb-8 text-gray-100">
+        Available Courses
+      </h1>
+      <div className="grid grid-cols-1 h-full overflow-y-scroll scrollbar-hide pt-4 w-full sm:grid-cols-2 lg:grid-cols-4 lg:gap-6 gap-2">
+        {Array.from({ length: 10 }).map((_, i) => (
+          <div className="w-full lg:aspect-square h-32 lg:h-full" key={i}>
+            <Card
+              courseName={`Subject ${i}`}
+              bannerImgSrc={`https://loremflickr.com/200/200?random=${i - 1}`}
+              courseType={"Web Development"}
+              educatorAvatar={"https://avatar.iran.liara.run/public/38"}
+            />
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
